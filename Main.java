4c4fdf1c3d44d482ec8db8ed257a116ca5ca4736
Wiktor Kosiński <wiki.kosinski@gmail.com>
2024-04-07 12:31:17 +0200

    zad4

--- Main.java
+++ Main.java
@@ -1,17 +1,27 @@
-
+import geometry.Circle;
 import geometry.ColoredCircle;
 import geometry.Point;
 
 public class Main {
     public static void main(String[] args) {
-        Point center = new Point(0, 0);
+        Circle[] circles = new Circle[3];
+        circles[0] = new Circle(new Point(0, 0), 5.0);
+        circles[1] = new Circle(new Point(2, 2), 3.0);
+        circles[2] = new Circle(new Point(-3, 1), 4.0);
 
-        ColoredCircle coloredCircle = new ColoredCircle(center, 9.0, "blue");
+        ColoredCircle[] coloredCircles = new ColoredCircle[2];
+        coloredCircles[0] = new ColoredCircle(new Point(1, -1), 6.0, "red");
+        coloredCircles[1] = new ColoredCircle(new Point(-2, -2), 2.5, "green");
 
-        System.out.println("Właściwości kolorowego koła:");
-        System.out.println("Środek koła: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
-        System.out.println("Promień koła: " + coloredCircle.getRadius());
-        System.out.println("Kolor koła: " + coloredCircle.getColor());
-        System.out.println("Obwód koła: " + coloredCircle.calculatePerimeter());
+        System.out.println("Obwody kół:");
+        for (Circle circle : circles) {
+            System.out.println("Obwód: " + circle.calculatePerimeter());
+        }
+        
+        System.out.println("\nObwody kolorowych kół:");
+        for (ColoredCircle coloredCircle : coloredCircles) {
+            System.out.println("Obwód: " + coloredCircle.calculatePerimeter());
+            System.out.println("Kolor: " + coloredCircle.getColor());
+        }
     }
 }
