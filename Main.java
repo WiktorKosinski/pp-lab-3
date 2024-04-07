ab3b6bc24a7f9b7ad6febc702ca615702f7ed9d0
Wiktor Kosiński <wiki.kosinski@gmail.com>
2024-04-07 12:05:28 +0200

    jakiś opis zmiany

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(4.5, 3.0);
+
+        System.out.println("Współrzędne punktu:");
+        System.out.println("x = " + point.getX());
+        System.out.println("y = " + point.getY());
+    }
+}
